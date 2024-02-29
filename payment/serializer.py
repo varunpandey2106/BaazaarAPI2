c301903affc0353ffe7e1b860ce30765732ef097
varunpandey2106 <varun.pandey2106@gmail.com>
2024-02-29 18:44:32 +0530

    payment serializer

---
+++ payment/serializer.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+
+from .models import Payment
+
+
+class ReadPaymentSerializer(serializers.ModelSerializer):
+    """Serializer class of Payment model for reading payment [GET]"""
+
+    class Meta:
+        model = Payment
+        fields = ["order_id", "amount", "status", "created_at", "updated_at"]
+
+    order_id = serializers.IntegerField(source="order.id")
+    created_at = serializers.StringRelatedField(source="order.created_at")
+
+
+class WritePaymentSerializer(serializers.ModelSerializer):
+    """Serializer class of Review model for writing payment [PATCH]"""
+
+    class Meta:
+        model = Payment
+        fields = ["status"]
