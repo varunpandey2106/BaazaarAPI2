23ea2aa47ed4251bf4a210e0e437265406055089
varunpandey2106 <varun.pandey2106@gmail.com>
2024-03-10 20:56:17 +0530

    SETTINGS

--- base/settings.py
+++ base/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7s*fi42m)2%wd)9c61dltb70as%)ev7nq4w!34twuoqr2rwly8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost','.vercel.app']
+ALLOWED_HOSTS = []
 
 
 
