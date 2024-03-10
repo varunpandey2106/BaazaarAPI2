5c8d814bf2efc7ce72f1d3e187b371ca03b78f1e
varunpandey2106 <varun.pandey2106@gmail.com>
2024-03-10 21:00:11 +0530

    SETTINGS

--- base/settings.py
+++ base/settings.py
@@ -154,12 +154,12 @@ import os
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DATABASE_ENGINE'),
-        'NAME': os.getenv('DATABASE_NAME'),
-        'USER': os.getenv('DATABASE_USER'),
-        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-        'HOST': os.getenv('DATABASE_HOST'),
-        'PORT': os.getenv('DATABASE_PORT'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'IZWhCNDOyHeBdtxfLvhAmwsBfhDaVGAS',
+        'HOST': 'viaduct.proxy.rlwy.net',
+        'PORT': 42647,
     }
 }
 
